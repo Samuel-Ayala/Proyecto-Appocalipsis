e7976d34150ab7d530bef54aa2e85261b2bb8958
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-11-01 23:08:11 -0500

    login y registro (faltan detalles)

--- app/src/main/java/pe/edu/pucp/proyecto1_appocalipsis/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/proyecto1_appocalipsis/MainActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import pe.edu.pucp.proyecto1_appocalipsis.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
